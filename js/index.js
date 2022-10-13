0c0ddcc0c22f7841b158de30471f9858c1b6499c
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-10-13 18:55:18 +0200

    Road correctly moving

--- js/index.js
+++ js/index.js
@@ -3,12 +3,16 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 const roadImg = new Image();
-canvas.style.border = "2px solid black";
 roadImg.src = "../images/road.png"
+const roadImg2 = new Image();
+roadImg2.src = "../images/road.png"
 const carImg = new Image ();
 carImg.src = "../images/car.png";
 let cvWidth = canvas.width;
 let cvHeight = canvas.height;
+let roadStartPosition = 0;
+let roadStartPosition2 = -cvHeight;
+let roadMove = 1;
 let carWidth = 50;
 let carHeight = 100;
 let XStartPosition = (cvWidth/2)-(carWidth/2);
@@ -24,18 +28,43 @@ window.onload = () => {
   };
 
   // GET BACKGROUND IMAGE
-  ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+  const drawRoad = () => {ctx.drawImage(roadImg, 0, roadStartPosition, canvas.width, canvas.height)};
+  const drawRoad2 = () => {ctx.drawImage(roadImg2, 0, roadStartPosition2, canvas.width, canvas.height)};
+ drawRoad();
 
   // FUNCTIONS
   const animate = () => {
-clearCar();
-ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
-moveCar();
+clearCanvas();
+drawRoad();
+moveRoad();
+drawRoad2();
+moveRoad2();
 drawCar();
+moveCar();
 requestAnimationFrame(animate);
   };
 
-const clearCar = () => {ctx.clearRect(0, 0, cvWidth, cvHeight)};
+const clearCanvas = () => {ctx.clearRect(0, 0, cvWidth, cvHeight)};
+
+const moveRoad = () => {
+  let roadCurrentPosition = roadStartPosition += roadMove;
+if (roadStartPosition < cvHeight) {
+roadCurrentPosition;
+}
+else {
+  roadStartPosition = 0;
+}
+};
+
+const moveRoad2 = () => {
+  let roadCurrentPosition2 = roadStartPosition2 += roadMove;
+if (roadStartPosition2 < 0) {
+roadCurrentPosition2;
+}
+else {
+  roadStartPosition2 = -cvHeight;
+}
+};
 
 const moveCar = () => {
     if(XStartPosition + XCarMove <= cvWidth - carWidth - 60 && XStartPosition + XCarMove >= 0 + carWidth + 10) 
