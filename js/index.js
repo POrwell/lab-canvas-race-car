0593ca20c8dc9d3532643a5f5970d28bb33b4b05
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-10-13 14:39:25 +0200

    "Iteration1 save"

--- js/index.js
+++ js/index.js
@@ -3,5 +3,11 @@ window.onload = () => {
     startGame();
   };
 
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+  const img = new Image();
+  img.src = "../" 
+  
+
   function startGame() {}
 };
